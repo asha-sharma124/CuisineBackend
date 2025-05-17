0fa11d9bbab759022ec0ab4f06ab04369a9e6583
asha <ashasharma08708@gmail.com>
2025-05-17 21:26:04 +0530

    m2

--- foodsite/foodsite/settings.py
+++ foodsite/foodsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["13.127.249.190","localhost"]
 AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend']
 
 
