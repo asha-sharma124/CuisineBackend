96bc8ba0f016ca09cc724fa452b1d80e6d3bb49b
asha <ashasharma08708@gmail.com>
2025-05-17 20:22:03 +0530

    new commit

--- foodsite/foodsite/settings.py
+++ foodsite/foodsite/settings.py
@@ -171,7 +171,7 @@ import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
