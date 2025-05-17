11e50614273ae31df9bb8c7d8ec4fd8ec127cbe4
asha <ashasharma08708@gmail.com>
2025-05-17 18:14:48 +0530

    commit pushed data1

--- foodsite/foodsite/settings.py
+++ foodsite/foodsite/settings.py
@@ -114,7 +114,7 @@ SIMPLE_JWT = {
 # }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
