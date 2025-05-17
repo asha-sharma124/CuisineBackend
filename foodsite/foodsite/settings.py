f523557a2bd98125bca84400cf6d77fdc72bf31e
asha <ashasharma08708@gmail.com>
2025-05-17 20:31:09 +0530

    new commit 3

--- foodsite/foodsite/settings.py
+++ foodsite/foodsite/settings.py
@@ -171,11 +171,11 @@ import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
