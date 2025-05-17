ad84e2d620081c8f8c0e49d6a5249fd55b871e05
asha <ashasharma08708@gmail.com>
2025-05-17 20:41:03 +0530

    new commit 4

--- foodsite/foodsite/settings.py
+++ foodsite/foodsite/settings.py
@@ -176,8 +176,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+import os
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
